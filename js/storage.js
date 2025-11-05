48e26644a49180efcc1d77792cb86d196bc03c84
suhailahamed2511 <suhailahamed2511@gmail.com>
2025-11-05 14:45:42 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -22,9 +22,10 @@ function seedDemoData(){
   const cur = loadEmployees();
   if(cur.length > 0) return;
   const demo = [
-    { id: generateId(), name: 'Asha Patel', department: 'IT', email: 'asha.patel@example.com', phone: '9876501234', designation: 'Frontend Dev', joiningDate: '2023-02-15', profileImage: null },
-    { id: generateId(), name: 'Ravi Kumar', department: 'HR', email: 'ravi.kumar@example.com', phone: '9876505678', designation: 'HR Manager', joiningDate: '2022-11-01', profileImage: null },
-    { id: generateId(), name: 'Neha Singh', department: 'Finance', email: 'neha.singh@example.com', phone: '9876509999', designation: 'Accountant', joiningDate: '2021-09-23', profileImage: null }
+    { id: generateId(), name: 'Suhail Ahmed', department: 'IT', email: 'suhail_ahmed@gmail.com', phone: '9876501234', designation: 'Frontend Dev', joiningDate: '2023-02-15', profileImage: null },
+    { id: generateId(), name: 'Ajith kumar', department: 'HR', email: 'ajith.kumar@gmail.com', phone: '9876505678', designation: 'HR Manager', joiningDate: '2022-11-01', profileImage: null },
+    { id: generateId(), name: 'Vijay Kumar', department: 'Finance', email: 'vijay_kumar@gmail.com', phone: '9876509999', designation: 'Accountant', joiningDate: '2021-09-23', profileImage: null }
   ];
   saveEmployees(demo);
 }
+
